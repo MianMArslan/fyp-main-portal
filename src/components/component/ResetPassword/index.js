8cca8e40ed3a9c118ae5c4592d2fb3dd776576eb
idevnerds-Arslan <arslan@idevnerds.com>
2022-04-02 00:30:45 +0500

    update input field in reset password form

--- src/components/component/ResetPassword/index.js
+++ src/components/component/ResetPassword/index.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import validate from "./validateInfo";
 import useForm from "./usereset";
 import "../loginform/login.css";
+import { TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import img1 from "../../images/off the beaten track.png";
 import { POST } from "../../../services/httpClient.js";
 import { useSearchParams, Navigate } from "react-router-dom";
@@ -39,6 +42,12 @@ const ResetPassword = ({ submitForm }) => {
     setOpen(false);
   };
 
+  const [state, setState] = useState(false);
+
+  const toggleBtn = () => {
+    setState((prevState) => !prevState);
+  };
+
   const validateRequest = (values) => {
     setLoading(true);
     if (!values.password) {
@@ -68,10 +77,10 @@ const ResetPassword = ({ submitForm }) => {
       }, 1000);
       setOpen(true);
       setLoading(false);
-      setsnakbarMessage("Email is successfully sent!");
+      setsnakbarMessage("Successfully Updated!");
     } else {
       setType("error");
-      setsnakbarMessage(res);
+      setsnakbarMessage(res.data.message);
       setOpen(true);
       setLoading(false);
     }
@@ -85,17 +94,29 @@ const ResetPassword = ({ submitForm }) => {
           <div className="login-content-right">
             <form onSubmit={handleSubmit} className="login" noValidate>
               <h1>Reset Password</h1>
-              <div className="login-inputs">
-                <label className="login-label">Reset Password</label>
-                <input
-                  className="login-input"
-                  type="password"
-                  name="password"
-                  placeholder="Enter your password"
-                  value={values.email}
-                  onChange={handleChange}
-                />
-              </div>
+              <Box
+                sx={{
+                  m: 1,
+                  width: 550,
+                  maxWidth: "100%",
+                }}
+              >
+                <div className="login-inputs">
+                  <TextField
+                    label="Password"
+                    type={state ? "text" : "password"}
+                    name="password"
+                    placeholder="Enter your password"
+                    fullWidth
+                    variant="standard"
+                    value={values.password}
+                    onChange={handleChange}
+                  />
+                  <button className="login-btn" onClick={toggleBtn}>
+                    {state ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+                  </button>
+                </div>
+              </Box>
               <button
                 className="login-input-btn"
                 type="submit"
