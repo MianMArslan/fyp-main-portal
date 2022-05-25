8f88b1fa8c344f5abf9d19d96a08df658a562920
idevnerds-Arslan <arslan@idevnerds.com>
2022-05-25 23:01:34 +0500

    integrate api for contactUs form update view

--- src/components/component/ContactUs/index.js
+++ src/components/component/ContactUs/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {POST} from "../../../services/httpClient";
+import { POST } from "../../../services/httpClient";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Snackbar from "../snakebar/index";
 import Aos from "aos";
@@ -15,37 +15,33 @@ import {
   ImgWrap,
   TextWrapper,
   InfoWrapper,
-  InfoRow
+  InfoRow,
 } from "./contactElements";
 
 import img1 from "../../images/off the beaten track.png";
 import { TextField, Box } from "@mui/material";
 
-
 export const homeObjfour = {
-id: "contact",
+  id: "contact",
   lightBg: false,
   lightText: true,
   topLine: "OFF THE BEATEN TRACK",
   headline: "Contact US",
   imgStart: false,
 };
-const ContactUs = ({
-    lightBg, lightText, id, imgStart, headline, topLine
-
-}) => {
+const ContactUs = ({ lightBg, lightText, id, imgStart, headline, topLine }) => {
   useEffect(() => {
     Aos.init({ duration: 3000 });
   }, []);
 
   const initialValues = {
     name: "",
-    suggestion:"",
+    suggestion: "",
     email: "",
   };
   const [formValues, setFormValues] = useState(initialValues);
   const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false)
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -77,100 +73,99 @@ const ContactUs = ({
       setType("error");
       setsnakbarMessage("This is not a valid email format!");
       setOpen(true);
-    }
-     else return true;
+    } else return true;
   };
   const create = async (formValues) => {
-      setLoading(true);
-      let res = await POST ("hello", formValues);
-      if (res?.code === 200) {
-        setType("success");
-        setOpen(true);
-        setLoading(false);
-        setsnakbarMessage(res?.message);
-        resetForm();
-      } else {
-        setType("error");
-        setOpen(true);
-        setLoading(false);
-        setsnakbarMessage(res?.message);
-        resetForm();
-      }
+    setLoading(true);
+    let res = await POST("/contactUs/all", formValues);
+    if (res?.code === 200) {
+      setType("success");
+      setOpen(true);
+      setLoading(false);
+      setsnakbarMessage("Suggestion Send Successfully!");
+      resetForm();
+    } else {
+      setType("error");
+      setOpen(true);
+      setLoading(false);
+      setsnakbarMessage(res?.message);
+      resetForm();
+    }
   };
   return (
     <>
       <InfoContainer lightBg={lightBg} id={id}>
         <InfoWrapper>
-          <InfoRow imgStart = {imgStart}>
-          <Column1>
+          <InfoRow imgStart={imgStart}>
+            <Column1>
               <TextWrapper data-aos="zoom-in">
                 <TopLine>{topLine}</TopLine>
                 <Heading lightText={lightText}>{headline}</Heading>
                 <form onSubmit={handleSubmit}>
-               <Box sx={{ width: 650, maxWidth: "100%" }}>
-              <TextField
-                label="Name"
-                type="name"
-                fullWidth
-                name="name"
-                placeholder="Enter your name"
-                variant="standard"
-                value={formValues.name}
-                onChange={handleChange}
-              />
-            <TextField
-                label="Email"
-                type="email"
-                fullWidth
-                name="email"
-                placeholder="Enter your email"
-                variant="standard"
-                value={formValues.email}
-                onChange={handleChange}
-              />
-              <TextField
-                label="Suggestion"
-                type="suggestion"
-                name="suggestion"
-                multiline
-                fullWidth
-                rows={10}
-                variant="standard"
-                placeholder="Enter your suggestion"
-                value={formValues.suggestion}
-                onChange={handleChange}
-              />
-            </Box>
-            <Button
-            type="submit"
-            onClick={async () => {
-              let validation = validate(formValues);
-              if (validation) await create(formValues);
-            }}
-          >
-            {isloading && <CircularProgress />}
-            {!isloading && <span>Send</span>}
-          </Button>
-            </form>
+                  <Box sx={{ width: 650, maxWidth: "100%" }}>
+                    <TextField
+                      label="Name"
+                      type="name"
+                      fullWidth
+                      name="name"
+                      placeholder="Enter your name"
+                      variant="standard"
+                      value={formValues.name}
+                      onChange={handleChange}
+                    />
+                    <TextField
+                      label="Email"
+                      type="email"
+                      fullWidth
+                      name="email"
+                      placeholder="Enter your email"
+                      variant="standard"
+                      value={formValues.email}
+                      onChange={handleChange}
+                    />
+                    <TextField
+                      label="Suggestion"
+                      type="suggestion"
+                      name="suggestion"
+                      multiline
+                      fullWidth
+                      rows={10}
+                      variant="standard"
+                      placeholder="Enter your suggestion"
+                      value={formValues.suggestion}
+                      onChange={handleChange}
+                    />
+                  </Box>
+                  <Button
+                    type="submit"
+                    onClick={async () => {
+                      let validation = validate(formValues);
+                      if (validation) await create(formValues);
+                    }}
+                  >
+                    {isloading && <CircularProgress />}
+                    {!isloading && <span>Submit</span>}
+                  </Button>
+                </form>
               </TextWrapper>
-              </Column1>
-              <Column2>
-               <ImgWrap data-aos="zoom-in">
-                 <Img src={img1} alt="" />
-               </ImgWrap>
-             </Column2>
-             </InfoRow>
-             </InfoWrapper>
-             </InfoContainer>
-       {open && (
-         <Snackbar
-           open={open}
-           setOpen={setOpen}
-           type={type}
-           message={snakbarMessage}
-         />
-       )}
-     </>
-   );
- };
+            </Column1>
+            <Column2>
+              <ImgWrap data-aos="zoom-in">
+                <Img src={img1} alt="" />
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+      {open && (
+        <Snackbar
+          open={open}
+          setOpen={setOpen}
+          type={type}
+          message={snakbarMessage}
+        />
+      )}
+    </>
+  );
+};
 export default ContactUs;
